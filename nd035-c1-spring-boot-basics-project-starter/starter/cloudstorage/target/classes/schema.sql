3cd4c25510cf98a15ebadf71cb587d11af343ca6
mojtabasadeghi <47910283+mojtabasadeghi@users.noreply.github.com>
2021-01-09 19:44:27 -0800

    Add files via upload

--- nd035-c1-spring-boot-basics-project-starter/starter/cloudstorage/target/classes/schema.sql
+++ nd035-c1-spring-boot-basics-project-starter/starter/cloudstorage/target/classes/schema.sql
@@ -2,8 +2,8 @@
 CREATE TABLE IF NOT EXISTS USERS (
   userid INT PRIMARY KEY auto_increment,
   username VARCHAR(20),
-  salt VARCHAR,
-  password VARCHAR,
+  salt VARCHAR(100),
+  password VARCHAR(100),
   firstname VARCHAR(20),
   lastname VARCHAR(20)
 );
@@ -18,9 +18,9 @@ CREATE TABLE IF NOT EXISTS NOTES (
 
 CREATE TABLE IF NOT EXISTS FILES (
     fileId INT PRIMARY KEY auto_increment,
-    filename VARCHAR,
-    contenttype VARCHAR,
-    filesize VARCHAR,
+    filename VARCHAR(150),
+    contenttype VARCHAR(50),
+    filesize VARCHAR(100),
     fk_userid INT,
     filedata mediumblob,
     instime datetime,
@@ -30,9 +30,9 @@ CREATE TABLE IF NOT EXISTS FILES (
 CREATE TABLE IF NOT EXISTS CREDENTIALS (
     credentialid INT PRIMARY KEY auto_increment,
     url VARCHAR(100),
-    username VARCHAR (30),
-    key VARCHAR,
-    password VARCHAR,
+    username VARCHAR(30),
+    key VARCHAR(150),
+    password VARCHAR(150),
     fk_userid INT,
     foreign key (fk_userid) references USERS(userid)
 );
