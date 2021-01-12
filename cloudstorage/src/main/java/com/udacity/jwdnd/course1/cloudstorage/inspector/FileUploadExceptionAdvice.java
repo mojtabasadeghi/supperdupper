6124fa2f0ee761dde7ba384c3ccb98f6be98db9f
mojtabasadeghi <47910283+mojtabasadeghi@users.noreply.github.com>
2021-01-11 17:47:52 -0800

    Add files via upload

--- cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/inspector/FileUploadExceptionAdvice.java
+++ cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/inspector/FileUploadExceptionAdvice.java
@@ -4,9 +4,11 @@ import com.udacity.jwdnd.course1.cloudstorage.model.Credentials;
 import com.udacity.jwdnd.course1.cloudstorage.model.File;
 import com.udacity.jwdnd.course1.cloudstorage.model.Notes;
 import com.udacity.jwdnd.course1.cloudstorage.services.CredentialsService;
+import com.udacity.jwdnd.course1.cloudstorage.services.ErrorMsgService;
 import com.udacity.jwdnd.course1.cloudstorage.services.NoteService;
 import com.udacity.jwdnd.course1.cloudstorage.services.UserService;
 import com.udacity.jwdnd.course1.cloudstorage.storage.StorageService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -21,6 +23,8 @@ import java.util.List;
 @ControllerAdvice
 public class FileUploadExceptionAdvice {
 
+    @Autowired
+    ErrorMsgService errorMsgService;
     @ExceptionHandler(MaxUploadSizeExceededException.class)
     public ModelAndView handleMaxSizeException(
             MaxUploadSizeExceededException exc,
@@ -28,7 +32,7 @@ public class FileUploadExceptionAdvice {
             HttpServletResponse response) {
 
         ModelAndView modelAndView = new ModelAndView("overmaxFilesize");
-        modelAndView.addObject("fileErrorMeesage", "Exceed File size! file size must be less than 2 MB");
+        modelAndView.addObject("fileErrorMeesage", errorMsgService.getFileSizeLimitError());
         return modelAndView;
     }
 
